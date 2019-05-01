c0f6fef8f968bd809882addc12bdc2fa443c0234
Ponchimeow <trancetoradora@gmail.com>
2019-05-01 22:43:29 +0800

    week3 hw1-hw5 ,hw1-hw5.test.js ,hw6.md and package.json

--- homeworks/week3/hw3.test.js
+++ homeworks/week3/hw3.test.js
@@ -4,4 +4,10 @@ describe('hw3', () => {
   it('should return correct answer when n = 1', () => {
     expect(isPrime(1)).toBe(false);
   });
+  it('should return correct answer when n = 37', () => {
+    expect(isPrime(37)).toBe(true);
+  });
+  it('should return correct answer when n = 544', () => {
+    expect(isPrime(544)).toBe(false);
+  });
 });
--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,24 @@
-function isPalindromes(str) {
-  console.log(str);
+// 前後比對
+
+function isPalindromes(s) {
+  const num = s.length;
+  for (let i = 0; i < num; i += 1) {
+    if (s.charAt(i) !== s.charAt(num - (i + 1))) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isPalindromes;
+
+/* 反轉
+function isPalindromes(s) {
+    let str = s.split('').reverse().join('');
+    let str_ES6 = [...s].reverse().join('');    //rest operator
+    if (s === (str||str_ES6)) {
+        return true;
+    }
+    return false;
+}
+*/
