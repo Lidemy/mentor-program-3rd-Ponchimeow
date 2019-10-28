c7933bd0d892c078a32314262398a4c8a9de5855
Ponchimeow <trancetoradora@gmail.com>
2019-10-28 15:19:37 +0800

    week21 sec commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,24 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    "eslint:recommended",
+    "plugin:react/recommended",
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['react'],
   rules: {
-    "linebreak-style": 0
+    "linebreak-style": 0,
+    'class-methods-use-this': 'off',
   },
 };
