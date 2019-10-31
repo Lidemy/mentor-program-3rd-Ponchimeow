61cc36ad12bc86aec458c6b20313f1d9aab1721d
Ponchimeow <trancetoradora@gmail.com>
2019-10-31 14:25:26 +0800

    hw1 done

--- homeworks/week21/fe/hw1/src/App.js
+++ homeworks/week21/fe/hw1/src/App.js
@@ -10,49 +10,67 @@ class Title extends Component {
   }
 }
 
-class App extends Component { 
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       todos: [
       ],
-      todoText:'', 
+      todoText: '',
       filter: 'all',
     }
 
     this.id = 1
-    this.handleChange =  this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
     this.addTodo = this.addTodo.bind(this)
     this.deleteTodo = this.deleteTodo.bind(this)
     this.markTodo = this.markTodo.bind(this)
+    this.updateTodo = this.updateTodo.bind(this)
   }
 
-  handleChange(e){
-     this.setState({
-        todoText: e.target.value
-     })
+  componentDidMount() {
+    const todoData = window.localStorage.getItem('todoapp');
+    if (todoData) {
+      const oldTodos = JSON.parse(todoData)
+      this.setState({
+        todos: oldTodos,
+      })
+      this.id = oldTodos[oldTodos.length - 1].id + 1
+    }
   }
 
-  addTodo(){
-     const {todos, todoText} = this.state;
-     this.setState({
-       todos: [...todos, {
-         id: this.id,
-         content: todoText,
-         status: false,
-       }],
-       todoText: '',
-     })
-     this.id ++
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      window.localStorage.setItem('todoapp', JSON.stringify(this.state.todos));
+    }
   }
 
-  deleteTodo(id){
+  handleChange(e) {
     this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id) 
+      todoText: e.target.value
     })
   }
 
-  markTodo(id){
+  addTodo() {
+    const { todos, todoText } = this.state;
+    this.setState({
+      todos: [...todos, {
+        id: this.id,
+        content: todoText,
+        status: false,
+      }],
+      todoText: '',
+    })
+    this.id++
+  }
+
+  deleteTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    })
+  }
+
+  markTodo(id) {
     this.setState({
       todos: this.state.todos.map((todo) => {
         if (todo.id !== id) {
@@ -66,47 +84,73 @@ class App extends Component {
     })
   }
 
-  filterTodo(filters,todos){
+  updateTodo(id, value) {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id !== id) {
+          return todo;
+        }
+        return {
+          ...todo,
+          content: value,
+        }
+      })
+    })
+  }
+
+  filterTodo(filters, todos) {
     switch (filters) {
       case 'done':
-      return todos.filter(todo=>todo.status);
+        return todos.filter(todo => todo.status);
       case 'undone':
-      return todos.filter(todo=>!todo.status);  
+        return todos.filter(todo => !todo.status);
       default:
-      return todos;
+        return todos;
     }
   }
 
   render() {
-    const { todos, todoText,filter } = this.state;
+    const { todos, todoText, filter } = this.state;
     return (
       <div className="container">
         <Title />
         <div className="list-header">
-          <input type="text" id="input-todo" placeholder="type in something" value={todoText} onChange={this.handleChange}/>
+          <input type="text" id="input-todo" placeholder="type in something" value={todoText} onChange={this.handleChange} />
           <div className="btn btn-add" type="button" onClick={this.addTodo}>add</div>
         </div>
         <div className="list-group">
-          <div className>
-            <div className="filters__all" onClick={()=>{
+          <div className="filters">
+            <div className="filter filter__all" onClick={() => {
               this.setState({
                 filter: 'all'
               })
-            }}>All</div>
-            <div className="filters__done" onClick={()=>{
+            }}>All
+              <span className="count">
+                {todos.length}
+              </span>
+            </div>
+            <div className="filter filter__done" onClick={() => {
               this.setState({
                 filter: 'done'
               })
-            }}>Done</div>
-            <div className="filters__undone" onClick={()=>{
+            }}>Done
+              <span className="count">
+                {(todos.filter(todo => todo.status)).length}
+              </span>
+            </div>
+            <div className="filter filter__undone" onClick={() => {
               this.setState({
                 filter: 'undone'
               })
-            }}>Undone</div>
+            }}>Undone
+              <span className="count">
+                {(todos.filter(todo => !todo.status)).length}
+              </span>
+            </div>
           </div>
-        {this.filterTodo(filter,todos).map(todo => (
-          <Todo key={todo.id} todo={todo} deleteTodo={this.deleteTodo}
-            markTodo={this.markTodo}/>
+          {this.filterTodo(filter, todos).map(todo => (
+            <Todo key={todo.id} todo={todo} deleteTodo={this.deleteTodo}
+              markTodo={this.markTodo} updateTodo={this.updateTodo} />
           ))}
         </div>
       </div>
@@ -115,92 +159,3 @@ class App extends Component {
 }
 
 export default App;
-        
-        /*
-  if (todos.length >= 1) {
-                id = todos[todos.length - 1].id + 1;
-              }
-            }
-  
-function setData() {
-                window.localStorage.setItem('todoapp', JSON.stringify(todos));
-              render(todos);
-            }
-  
-$(document).ready(() => {
-  const todoData = window.localStorage.getItem('todoapp');
-  if (todoData) {
-                todos = JSON.parse(todoData);
-              render(todos);
-            }
-  
-  $('.list-group').click((e) => {
-    const dataId = Number($(e.target).attr('data-id'));
-          
-                      // 事項狀態
-    if ($(e.target).hasClass('checkbox')) {
-                todos = todos.map((todo) => {
-                  if (todo.id !== dataId) {
-                    return todo;
-                  }
-                  return {
-                    ...todo,
-                    status: !todo.status,
-                  };
-                });
-              setData();
-            } */
-  
-  // 編輯
-// if ($(e.target).hasClass('btn-edit')) {
-//   if ($(e.target).parent().find('.checkbox').prop('checked')) {
-//     alert('已完成事項無法編輯');
-//     return;
-//   }
-//   $(e.target).prev().prev().children()
-//     .eq(0)
-//     .hide(); // item-content,label value
-//   $(e.target).prev().show(); // input
-//   $(e.target).hide(); // edit
-//   $(e.target).nextAll().eq(0).show(); // save
-//   $(e.target).nextAll().eq(0).addClass('active');
-//   $(e.target).nextAll().eq(1).hide(); // delete
-//   $(e.target).nextAll().eq(2).show(); // cancel
-//   $(e.target).nextAll().eq(2).addClass('active');
-// }
-
-// // 刪除
-// if ($(e.target).hasClass('btn-delete')) {
-//   // eslint-disable-next-line no-alert
-//   // eslint-disable-next-line no-restricted-globals
-//   if (confirm('確認要刪除?')) {
-//     todos = todos.filter(todo => todo.id !== dataId);
-//     setData();
-//   }
-// }
-
-// // 編輯後儲存
-// if ($(e.target).hasClass('btn-save')) {
-//   const content = $(e.target).parent().find('.item-input').val();
-//   if (content !== '') {
-//     todos = todos.map((todo) => {
-//       if (todo.id !== dataId) {
-//         return todo;
-//       }
-//       return {
-//         ...todo,
-//         content,
-//       };
-//     });
-//     setData();
-//   } else {
-//     alert('內容不可為空');
-//   }
-// }
-
-// // 取消編輯
-// if ($(e.target).hasClass('btn-cancel')) {
-//   setData();
-// }
-//           });
-//         });
--- homeworks/week21/fe/hw1/src/Todo.js
+++ homeworks/week21/fe/hw1/src/Todo.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isInEditMode: false,
+      editText: '',
+    }
 
-    this.delete = this.delete.bind(this);
     this.mark = this.mark.bind(this);
-  }
-
-  delete() {
-    const { todo, deleteTodo } = this.props;
-    deleteTodo(todo.id);
+    this.changeEditMode = this.changeEditMode.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.update = this.update.bind(this)
+    this.delete = this.delete.bind(this);
   }
 
   mark() {
@@ -19,32 +21,66 @@ class Todo extends Component {
     markTodo(todo.id);
   }
 
+  delete() {
+    const { todo, deleteTodo } = this.props;
+    deleteTodo(todo.id);
+  }
+
+  changeEditMode() {
+    const { isInEditMode } = this.state;
+    this.setState({
+      isInEditMode: !isInEditMode,
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      editText: e.target.value,
+    })
+  }
+
+  update() {
+    const { todo, updateTodo } = this.props;
+    const { editText } = this.state;
+    this.setState({
+      isInEditMode: false,
+      editText: '',
+    })
+    if (editText !== '') {
+      updateTodo(todo.id, editText);
+    }
+  }
+
   render() {
     const { todo } = this.props;
+    const { isInEditMode } = this.state;
     return (
       <li className="list-group-item">
         <label>
-          <span className="item-content">
+          <span className="item-content" style={{ display: isInEditMode ? 'none' : 'block' }}>
             {todo.content}
           </span>
           <input className="checkbox" type="checkbox" defaultChecked={todo.status} data-id={todo.id} onClick={this.mark} />
-          <span className="checkmark"></span>
+          <span className="checkmark" ></span>
         </label>
-        <input type="text" className="item-input" value={todo.content} />
-        <span className="btn btn-edit"></span>
-        <span className="btn btn-save" ></span>
-        <span className="btn btn-delete" onClick={this.delete}></span>
-        <span className="btn btn-cancel"></span>
-      </li>
+        <input type="text" className="item-input" defaultValue={todo.content} style={{ display: isInEditMode ? 'block' : 'none' }} onChange={this.handleChange} />
+        <span className="btn btn-edit" onClick={this.changeEditMode} style={{ display: isInEditMode ? 'none' : 'block' }}></span>
+        <span className="btn btn-save" onClick={this.update} style={{ display: isInEditMode ? 'block' : 'none' }}></span>
+        <span className="btn btn-delete" onClick={this.delete} style={{ display: isInEditMode ? 'none' : 'block' }}></span>
+        <span className="btn btn-cancel" onClick={this.changeEditMode} style={{ display: isInEditMode ? 'block' : 'none' }}></span>
+      </li >
 
     );
   }
 }
 
 Todo.propTypes = {
+  setState: PropTypes.func,
   todo: PropTypes.object,
   deleteTodo: PropTypes.func,
   markTodo: PropTypes.func,
+  updateTodo: PropTypes.func,
+  isInEditMode: PropTypes.string,
 }
 
 export default Todo;
